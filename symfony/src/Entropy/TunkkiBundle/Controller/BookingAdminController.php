4f0801d41c99f512153bf786bfe5fe271f36f9f6
Harri Häivälä <haivala@cc.hut.fi>
2017-10-19 19:29:00 +0300

    accessories

--- symfony/src/Entropy/TunkkiBundle/Controller/BookingAdminController.php
+++ symfony/src/Entropy/TunkkiBundle/Controller/BookingAdminController.php
@@ -15,8 +15,12 @@ class BookingAdminController extends CRUDController
         foreach ($object->getPakages() as $item){
             $pakages[]=$item;
         }
+        foreach ($object->getAccessories() as $item){
+            $accessories[]=$item;
+        }
         $data['items']=$items;
         $data['pakages']=$pakages;
+        $data['accessories']=$accessories;
         return $this->render('EntropyTunkkiBundle:BookingAdmin:stufflist.html.twig', $data);
     }
 
