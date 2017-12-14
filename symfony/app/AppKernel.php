8983a63193cf9e132f0718d71254c7121ffb4747
Harri Häivälä <haivala@cc.hut.fi>
2017-12-14 23:23:43 +0200

    upgrade

--- symfony/app/AppKernel.php
+++ symfony/app/AppKernel.php
@@ -1,5 +1,5 @@
 <?php
-
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
@@ -75,9 +75,25 @@ class AppKernel extends Kernel
 
         return $bundles;
     }
-
+    public function getRootDir()
+    {
+        return __DIR__;
+    }
+    public function getCacheDir()
+    {
+        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
+    }
+    public function getLogDir()
+    {
+        return dirname(__DIR__).'/var/logs';
+    }
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
+        $loader->load(function (ContainerBuilder $container) {
+            $container->setParameter('container.autowiring.strict_mode', true);
+            $container->setParameter('container.dumper.inline_class_loader', true);
+            $container->addObjectResource($this);
+        });
         $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
     }
 }
