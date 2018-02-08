16ddae94bd23fe5c30f2b7a33b01411d7e90df61
Harri Häivälä <haivala@cc.hut.fi>
2018-02-08 17:42:39 +0200

    stuff

--- symfony/src/Entropy/TunkkiBundle/Controller/BookingAdminController.php
+++ symfony/src/Entropy/TunkkiBundle/Controller/BookingAdminController.php
@@ -7,19 +7,24 @@ use Sonata\AdminBundle\Controller\CRUDController;
 class BookingAdminController extends CRUDController
 {
     public function stuffListAction() 
-    { 
+    {
+		$items = [];
+		$packages = [];
+		$accessories = [];
         $object = $this->admin->getSubject(); 
         foreach ($object->getItems() as $item){
             $items[]=$item;
         }
         foreach ($object->getPakages() as $item){
-            $pakages[]=$item;
+            $packages[]=$item;
         }
         foreach ($object->getAccessories() as $item){
             $accessories[]=$item;
         }
+		$data['name']=$object->getName();
+		$data['date']=$object->getBookingDate()->format('j.n.Y');
         $data['items']=$items;
-        $data['pakages']=$pakages;
+        $data['packages']=$packages;
         $data['accessories']=$accessories;
         return $this->render('EntropyTunkkiBundle:BookingAdmin:stufflist.html.twig', $data);
     }
