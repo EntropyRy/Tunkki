90734d08f18320cc33a7262e8150ba14f24f3028
Harri Häivälä <haivala@cc.hut.fi>
2018-04-05 22:51:37 +0300

    t

--- symfony/src/Entropy/TunkkiBundle/Controller/BookingAdminController.php
+++ symfony/src/Entropy/TunkkiBundle/Controller/BookingAdminController.php
@@ -8,30 +8,30 @@ class BookingAdminController extends CRUDController
 {
     public function stuffListAction() 
     {
-		$items = [];
-		$packages = [];
-		$accessories = [];
-		$rent['items'] = 0;
-		$rent['packages'] = 0;
-		$rent['accessories'] = 0;
+        $items = [];
+        $packages = [];
+        $accessories = [];
+        $rent['items'] = 0;
+        $rent['packages'] = 0;
+        $rent['accessories'] = 0;
         $object = $this->admin->getSubject(); 
         foreach ($object->getItems() as $item){
-			$items[]=$item;
-			$rent['items']+=$item->getRent();
+            $items[]=$item;
+            $rent['items']+=$item->getRent();
         }
         foreach ($object->getPackages() as $item){
             $packages[]=$item;
-			$rent['packages']+=$item->getRent();
+            $rent['packages']+=$item->getRent();
         }
         foreach ($object->getAccessories() as $item){
             $accessories[]=$item;
-			$rent['accessories']+=$item->getName()->getPrice()*$item->getCount();
-		}
-		$rent['total'] = $rent['items'] + $rent['packages']; //+ $rent['accessories'];
-		$rent['actualTotal']=$object->getActualPrice();
+            $rent['accessories']+=$item->getName()->getPrice()*$item->getCount();
+        }
+        $rent['total'] = $rent['items'] + $rent['packages']; //+ $rent['accessories'];
+        $rent['actualTotal']=$object->getActualPrice();
 
-		$data['name']=$object->getName();
-		$data['date']=$object->getBookingDate()->format('j.n.Y');
+        $data['name']=$object->getName();
+        $data['date']=$object->getBookingDate()->format('j.n.Y');
         $data['items']=$items;
         $data['packages']=$packages;
         $data['accessories']=$accessories;
