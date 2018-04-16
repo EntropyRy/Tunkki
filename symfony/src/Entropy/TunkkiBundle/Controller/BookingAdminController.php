8b9fa4a485646acdd460727486914a3ca9accfdf
Harri Häivälä <haivala@cc.hut.fi>
2018-04-16 17:07:04 +0300

    repo

--- symfony/src/Entropy/TunkkiBundle/Controller/BookingAdminController.php
+++ symfony/src/Entropy/TunkkiBundle/Controller/BookingAdminController.php
@@ -6,39 +6,14 @@ use Sonata\AdminBundle\Controller\CRUDController;
 
 class BookingAdminController extends CRUDController
 {
+    protected $em;
     public function stuffListAction() 
     {
-        $items = [];
-        $packages = [];
-        $accessories = [];
-        $rent['items'] = 0;
-        $rent['packages'] = 0;
-        $rent['accessories'] = 0;
-        $compensation['accessories'] = 0;
-        $object = $this->admin->getSubject(); 
-        foreach ($object->getItems() as $item){
-            $items[]=$item;
-            $rent['items']+=$item->getRent();
-        }
-        foreach ($object->getPackages() as $item){
-            $packages[]=$item;
-            $rent['packages']+=$item->getRent();
-        }
-        foreach ($object->getAccessories() as $item){
-            $accessories[]=$item;
-            $compensation['accessories']+=$item->getName()->getCompensationPrice()*$item->getCount();
-        }
-        $rent['total'] = $rent['items'] + $rent['packages']; //+ $rent['accessories'];
-        $rent['actualTotal']=$object->getActualPrice();
-
-        $data['name']=$object->getName();
-        $data['date']=$object->getBookingDate()->format('j.n.Y');
-        $data['items']=$items;
-        $data['packages']=$packages;
-        $data['accessories']=$accessories;
-        $data['rent']=$rent;
-        $data['compensation']=$compensation;
-        return $this->render('EntropyTunkkiBundle:BookingAdmin:stufflist.html.twig', $data);
+        $object = $this->admin->getSubject();
+        $this->em = $this->getDoctrine()->getManager();
+        $bookingdata = $this->em->getRepository('EntropyTunkkiBundle:Booking')
+              ->getBookingData($object->getId(), $object->getRenterHash(), $object->getRenter());
+        return $this->render('EntropyTunkkiBundle:BookingAdmin:stufflist.html.twig', $bookingdata);
     }
 
 }
--- symfony/src/Entropy/TunkkiBundle/Controller/RenterHashController.php
+++ symfony/src/Entropy/TunkkiBundle/Controller/RenterHashController.php
@@ -16,45 +16,13 @@ class RenterHashController extends Controller
         if(empty($bookingid) || empty($hash) || empty($renterid)){
             throw new NotFoundHttpException();
         }
-        $this->em = $this->container->get('doctrine.orm.entity_manager');
+        $this->em = $this->getDoctrine()->getManager();
         $renter = $this->em->getRepository('EntropyTunkkiBundle:Renter')
                 ->findOneBy(['id' => $renterid]);
-        $booking = $this->em->getRepository('EntropyTunkkiBundle:Booking')
-            ->findOneBy(['id' => $bookingid, 'renterHash' => $hash]);
-        if ( !empty($booking) and $booking->getRenter() == $renter){
-            $object = $booking;
-        $items = [];
-        $packages = [];
-        $accessories = [];
-        $rent['items'] = 0;
-        $compensation['items'] = 0;
-        $rent['packages'] = 0;
-        $rent['accessories'] = 0;
-        $compensation['accessories'] = 0;
-        foreach ($object->getItems() as $item){
-            $items[]=$item;
-            $rent['items']+=$item->getRent();
-            $compensation['items']+=$item->getCompensationPrice();
-        }
-        foreach ($object->getPackages() as $item){
-            $packages[]=$item;
-        }
-        foreach ($object->getAccessories() as $item){
-            $accessories[]=$item;
-            $compensation['accessories']+=$item->getName()->getCompensationPrice()*$item->getCount();
-        }
-        $rent['total'] = $rent['items'] + $rent['packages']; //+ $rent['accessories'];
-        $rent['actualTotal']=$object->getActualPrice();
-
-        $data['name']=$object->getName();
-        $data['date']=$object->getBookingDate()->format('j.n.Y');
-        $data['items']=$items;
-        $data['packages']=$packages;
-        $data['accessories']=$accessories;
-        $data['rent']=$rent;
-        $data['compensation']=$compensation;
-
-        return $this->render('EntropyTunkkiBundle::stufflist_for_renter.html.twig', $data);
+        $bookingdata = $this->em->getRepository('EntropyTunkkiBundle:Booking')
+            ->getBookingData($bookingid, $hash, $renter);
+        if (!empty($bookingdata)){
+            return $this->render('EntropyTunkkiBundle::stufflist_for_renter.html.twig', $bookingdata);
         }
         else {
             throw new NotFoundHttpException();
