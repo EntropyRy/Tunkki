568cb0b20066cc0b5a4a39664c39d42b574f2a58
Harri Häivälä <haivala@cc.hut.fi>
2018-08-08 19:42:42 +0300

    f

--- symfony/src/Entropy/TunkkiBundle/EventListener/BookingAdminListener.php
+++ symfony/src/Entropy/TunkkiBundle/EventListener/BookingAdminListener.php
@@ -11,38 +11,40 @@ class BookingAdminListener
     private $mailer = null;
     private $templating = null;
     private $email = null;
+    private $fromEmail = null;
     private $img = null;
 
-    public function __construct(\Swift_Mailer $mailer, $templating, $email, $img)
+    public function __construct(\Swift_Mailer $mailer, $templating, $email, $img,$fromEmail)
     {
         $this->mailer = $mailer;
         $this->templating = $templating;
         $this->email = $email;
         $this->img = $img;
+        $this->fromEmail = $fromEmail;
     }
 
     public function sendEmailNotification( \Sonata\AdminBundle\Event\PersistenceEvent $event )
     {       
         if($this->email){
-           $booking = $event->getObject();
-           //$results = [];
-           $mailer = $this->mailer;
-           $message = new \Swift_Message();
-           $message->setFrom([$this->email], "Tunkki");
-           $message->setTo($this->email);
-           $message->setSubject("[Entropy Tunkki] New Booking on ". $booking->getBookingDate()->format('d.m.Y') );
-           $message->setBody(
-           $this->templating->render(
-               'EntropyTunkkiBundle:Emails:notification.html.twig',
-                   [
-                       'img' => $this->img,
-                       'booking' => $booking,
-                   ] 
-               ),
-               'text/html'
-           );
-           //$results[] = 
-           $mailer->send($message);
+           if(get_class($event->getObject()) == "Entropy\TunkkiBundle\Entity\Booking"){
+               $booking = $event->getObject();
+               $mailer = $this->mailer;
+               $message = new \Swift_Message();
+               $message->setFrom([$this->fromEmail], "Tunkki");
+               $message->setTo($this->email);
+               $message->setSubject("[Entropy Tunkki] New Booking on ". $booking->getBookingDate()->format('d.m.Y') );
+               $message->setBody(
+               $this->templating->render(
+                   'EntropyTunkkiBundle:Emails:notification.html.twig',
+                       [
+                           'img' => $this->img,
+                           'booking' => $booking,
+                       ] 
+                   ),
+                   'text/html'
+               );
+               $mailer->send($message);
+           }
         }
     }
 }
