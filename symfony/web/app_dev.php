fa480a1b1d9e11520377b5be25593950633d237e
Harri Häivälä <haivala@cc.hut.fi>
2019-11-30 12:46:19 +0200

    syfony 4.4

--- symfony/web/app_dev.php
+++ symfony/web/app_dev.php
@@ -17,9 +17,6 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
 require __DIR__.'/../vendor/autoload.php';
 Debug::enable();
 $kernel = new AppKernel('dev', true);
-if (PHP_VERSION_ID < 70000) {
-    $kernel->loadClassCache();
-}
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
