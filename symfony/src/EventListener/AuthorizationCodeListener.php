092872b395f1ab08a2f8d8a9656cfbcad1a766e2
Harri Häivälä <haivala@cc.hut.fi>
2020-11-10 00:07:12 +0200

    yu

--- symfony/src/EventListener/AuthorizationCodeListener.php
+++ symfony/src/EventListener/AuthorizationCodeListener.php
@@ -1,25 +1,44 @@
 <?php
 declare(strict_types=1);
+
 namespace App\EventListener;
 use Nyholm\Psr7\Response;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Trikoder\Bundle\OAuth2Bundle\Event\AuthorizationRequestResolveEvent;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
 final class AuthorizationCodeListener
 {
     private $urlGenerator;
     private $requestStack;
+    private $session;
+
     public function __construct(
         UrlGeneratorInterface $urlGenerator,
-        RequestStack $requestStack
+        RequestStack $requestStack,
+        SessionInterface $session
     ) {
         $this->urlGenerator = $urlGenerator;
         $this->requestStack = $requestStack;
+        $this->session      = $session;
     }
     public function onAuthorizationRequestResolve(AuthorizationRequestResolveEvent $event)
     {
         if (null !== $event->getUser()) {
-            $event->resolveAuthorization(AuthorizationRequestResolveEvent::AUTHORIZATION_APPROVED);
+            if ($event->getUser()->getMember()->getIsActiveMember()){
+                $event->resolveAuthorization(AuthorizationRequestResolveEvent::AUTHORIZATION_APPROVED);
+            } else {
+                $this->session->getFlashbag()->add('warning', 'profile.only_for_active_members');
+
+                $event->setResponse(
+                    new Response(
+                        302,
+                        ['Location' => $this->urlGenerator->generate('entropy_profile.'. $event->getUser()->getMember()->getLocale())]
+                    )
+                );
+            }
+
         } else {
             $event->setResponse(
                 new Response(
