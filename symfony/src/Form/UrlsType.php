6f8b35263cd8611caa9460b67cc875fb7946d305
Harri Häivälä <haivala@cc.hut.fi>
2020-11-14 13:41:19 +0200

    update

--- symfony/src/Form/UrlsType.php
+++ symfony/src/Form/UrlsType.php
@@ -15,23 +15,22 @@ final class UrlsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('url', UrlType::class, [
+                'label' => 'url.form.url',
+                'attr' => ['placeholder' => 'https://soundcloud.com/entropy-fi']
+            ])
             ->add('icon', TextType::class, [
                 'label' => 'url.form.icon',
                 'help_html' => true,
-                'help' => '<i class="fas fa-music"></i> Check the <a target=_blank href="https://fontawesome.com/icons">icon list</a>',
+                'help' => '<a href="#"><i class="fas fa-music"></i></a> Check the <a target=_blank href="https://fontawesome.com/icons">icon list</a>',
                 'attr' => ['placeholder' => 'fab fa-soundcloud']
             ])
             ->add('title', TextType::class,[
                 'label' => 'url.form.title',
                 'attr' => ['placeholder' => 'Soundcloud']
             ])
-            ->add('url', UrlType::class, [
-                'label' => 'url.form.url',
-                'attr' => ['placeholder' => 'https://soundcloud.com/entropy-fi']
-            ])
             ->add('open_in_new_window', CheckboxType::class,[
                 'label'=> 'url.form.open_in_new_window',
-                'attr' => ['checked' => 'checked']
             ])
             ;
     }
