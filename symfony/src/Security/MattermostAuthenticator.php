8b5537af9db29e88ec75568d82a7f65e1ebc8eb6
Harri Häivälä <haivala@cc.hut.fi>
2021-10-19 16:52:33 +0300

    update

--- symfony/src/Security/MattermostAuthenticator.php
+++ symfony/src/Security/MattermostAuthenticator.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
