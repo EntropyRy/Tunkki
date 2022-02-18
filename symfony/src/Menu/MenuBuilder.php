60f0f834528b5af6ed238fd49744c1a45f5142a6
Harri Häivälä <haivala@cc.hut.fi>
2022-02-18 14:23:56 +0200

    fix

--- symfony/src/Menu/MenuBuilder.php
+++ symfony/src/Menu/MenuBuilder.php
@@ -128,7 +128,7 @@ class MenuBuilder
                 ['max_duration' => 4]
             );
             if ($response->getStatusCode() == 200) {
-                $menu->addChild('Stream is Live', ['uri' => 'https://stream.entropy.fi/'])->setLinkAttribute('class', 'hilight');
+                $menu->addChild('Stream', ['uri' => 'https://stream.entropy.fi/'])->setLinkAttribute('class', 'hilight');
             }
         } catch (TransportExceptionInterface $e) {
             return;
