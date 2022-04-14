c18ddea04fb49c16f8dacc4777adb14a5f8b6a1b
Harri Häivälä <haivala@cc.hut.fi>
2022-04-14 16:12:18 +0300

    smaller js and css

--- symfony/assets/js/app.js
+++ symfony/assets/js/app.js
@@ -1,10 +1,22 @@
-import '../css/custom.scss';
+import '../css/bs.scss';
 import '../css/polaroid.scss';
 import '../css/app.css';
 import '../css/dark.scss';
 import '../css/animations.scss';
 
-import 'bootstrap';
+//import 'bootstrap';
+import 'bootstrap/js/dist/alert';
+import 'bootstrap/js/dist/button';
+// import 'bootstrap/js/dist/carousel';
+import 'bootstrap/js/dist/collapse';
+import 'bootstrap/js/dist/dropdown';
+// import 'bootstrap/js/dist/modal';
+// import 'bootstrap/js/dist/popover';
+// import 'bootstrap/js/dist/scrollspy';
+import 'bootstrap/js/dist/tab';
+// import 'bootstrap/js/dist/toast';
+// import 'bootstrap/js/dist/tooltip';
+
 import bsCustomFileInput from 'bs-custom-file-input';
 import a2lix_lib from '@a2lix/symfony-collection/src/a2lix_sf_collection';
 
