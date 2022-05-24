b251a98bd65110f590b727dcdf6773bb0c2eb2ae
Harri Häivälä <haivala@cc.hut.fi>
2022-05-24 16:54:39 +0300

    Log Admin

---
+++ symfony/src/Admin/DoorLogAdmin.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+final class DoorLogAdmin extends AbstractAdmin
+{
+
+    protected function configureDatagridFilters(DatagridMapper $filter): void
+    {
+        $filter
+            ->add('member')
+            ->add('createdAt')
+            ->add('message')
+            ;
+    }
+
+    protected function configureListFields(ListMapper $list): void
+    {
+        $list
+            ->add('member')
+            ->add('createdAt')
+            ->add('message')
+/*            ->add(ListMapper::NAME_ACTIONS, null, [
+                'actions' => [
+                    'show' => [],
+                ],
+])*/;
+    }
+
+    protected function configureFormFields(FormMapper $form): void
+    {
+        $form
+            ->add('createdAt')
+            ->add('message')
+            ;
+    }
+
+    protected function configureShowFields(ShowMapper $show): void
+    {
+        $show
+            ->add('user')
+            ->add('createdAt')
+            ->add('message')
+            ;
+    }
+    public function configureRoutes(\Sonata\AdminBundle\Route\RouteCollection $collection)  {
+        $collection->remove('edit');
+        $collection->remove('delete');
+        $collection->remove('create');
+    }
+}
