1b997ec77c1f68616594a826c65267c290b70948
Harri Häivälä <haivala@cc.hut.fi>
2022-06-11 18:53:17 +0300

    fix

--- symfony/src/Menu/MenuBuilder.php
+++ symfony/src/Menu/MenuBuilder.php
@@ -37,7 +37,7 @@ class MenuBuilder
             $menu = $this->addItem($menu, $m, $locale);
             $m = $this->sortByPosition($m);
             foreach ($m as $item){
-                if($m->getEnabled()){
+                if($item->getEnabled()){
                     if($item->getUrl() == '#'){
                         $dropdown = $menu->addChild(
                             $item->getNimi(),
