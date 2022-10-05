bf7595b8a0b366d0164760c5c9bc9796ae46769e
Harri Häivälä <haivala@cc.hut.fi>
2022-10-05 18:57:44 +0300

    fix

--- symfony/src/Security/MattermostAuthenticator.php
+++ symfony/src/Security/MattermostAuthenticator.php
@@ -56,7 +56,7 @@ class MattermostAuthenticator extends OAuth2Authenticator implements Authenticat
                 // 2) do we have a matching user by email?
                 $member = $this->em->getRepository(Member::class)->findOneBy(['email' => $email]);
                 $user = $member->getUser();
-                $user->setMattermostId($mattermostUser->getId());
+                $user->setMattermostId($mmUser->getId());
                 $this->em->persist($user);
                 $this->em->flush();
 
