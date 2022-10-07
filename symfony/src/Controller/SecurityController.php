07e1a8e70d2b52534b758e164268388d5d412380
Harri Häivälä <haivala@cc.hut.fi>
2022-10-07 14:11:58 +0300

    redirect

--- symfony/src/Controller/SecurityController.php
+++ symfony/src/Controller/SecurityController.php
@@ -12,9 +12,9 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        if ($this->getUser()) {
+            return $this->redirectToRoute('entropy_user_dashboard.'.$this->getUser()->getMember()->getLocale());
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
