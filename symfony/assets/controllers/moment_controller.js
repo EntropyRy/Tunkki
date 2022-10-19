fe8af0bf94322a875dcbc08c72cade3e14372b9b
Harri Häivälä <haivala@cc.hut.fi>
2022-10-19 17:29:09 +0300

    moment

---
+++ symfony/assets/controllers/moment_controller.js
@@ -0,0 +1,32 @@
+import { Controller } from '@hotwired/stimulus';
+import moment from 'moment';
+
+export default class extends Controller {
+    static targets = ['badge']
+    static values = { locale: String, date: String, refreshInterval: Number }
+    connect() {
+        this.changeTime()
+        if (this.hasRefreshIntervalValue) {
+          this.startRefreshing()
+        }
+    }
+    disconnect() {
+        this.stopRefreshing()
+    }
+    changeTime(){
+      moment.locale(this.localeValue);
+      let date = moment(this.dateValue);
+      this.badgeTarget.innerText = date.fromNow();
+    }
+    startRefreshing() {
+      this.refreshTimer = setInterval(() => {
+        this.changeTime()
+      }, this.refreshIntervalValue)
+    }
+
+    stopRefreshing() {
+      if (this.refreshTimer) {
+        clearInterval(this.refreshTimer)
+      }
+    }
+}
