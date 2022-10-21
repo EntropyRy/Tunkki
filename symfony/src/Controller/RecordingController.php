eb69f088e09c6de8095e0824d1934e248a654423
Harri Häivälä <haivala@cc.hut.fi>
2022-10-21 13:40:21 +0300

    Stream commands

---
+++ symfony/src/Controller/RecordingController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Helper\SSH;
+/**
+ * @IsGranted("ROLE_USER")
+ */
+class RecordingController extends AbstractController
+{
+    #[Route('/kerde/recording/start', name: 'app_recording_start')]
+    public function start(Request $request, SSH $ssh): RedirectResponse
+    {
+        $member = $this->getUser()->getMember();
+        if ($member->getIsActiveMember()){
+            $err = $ssh->sendCommand('start');
+            if($err){
+                $this->addFlash('warning', 'Error: '.$err);
+            } else {
+                $this->addFlash('success', 'stream.command.successful');
+            }
+        }
+        return $this->redirectToRoute('entropy_profile_door.'. $request->getLocale());
+    }
+    #[Route('/kerde/recording/stop', name: 'app_recording_stop')]
+    public function stop(Request $request, SSH $ssh): RedirectResponse
+    {
+        $member = $this->getUser()->getMember();
+        if ($member->getIsActiveMember()){
+            $err = $ssh->sendCommand('stop');
+            if($err){
+                $this->addFlash('warning', 'Error: '.$err);
+            } else {
+                $this->addFlash('success', 'stream.command.successful');
+            }
+        }
+        return $this->redirectToRoute('entropy_profile_door.'. $request->getLocale());
+    }
+}
---
+++ symfony/src/Helper/SSH.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Helper;
+
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+class SSH
+{
+    public function __construct(protected ParameterBagInterface $bag)
+    {
+    }
+    public function sendCommand($text): string|bool
+    {
+        $connection = $this->getConnection();
+        if($text == 'start'){
+            $stream = ssh2_exec($connection, 'systemctl --user start es_streaming.target');
+        }
+        if($text == 'stop'){
+            $stream = ssh2_exec($connection, 'systemctl --user stop es_streaming.target');
+        }
+        $error = ssh2_fetch_stream($stream,SSH2_STREAM_STDERR);
+        $ret = stream_get_contents($error);
+        fclose($stream);
+        unset($connection);
+        return $ret;
+    }
+    protected function getConnection(): mixed
+    {
+        $connection = ssh2_connect($this->bag->get('recording.host'), $this->bag->get('recording.port') );
+        ssh2_auth_password($connection, $this->bag->get('recording.user'), $this->bag->get('recording.pass'));
+        return $connection;
+    }
+}
