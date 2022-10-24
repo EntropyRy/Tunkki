ea699e0ed27303cb8812f48a16783ce1440917cd
Harri Häivälä <haivala@cc.hut.fi>
2022-10-24 23:42:37 +0300

    nakkikone works

--- symfony/assets/js/nakkikone.js
+++ symfony/assets/js/nakkikone.js
@@ -1,10 +1,19 @@
 document.addEventListener('DOMContentLoaded',function(){
     var tabElms = document.querySelectorAll('a[data-bs-toggle="list"]')
+    var cards = document.querySelectorAll('.card-hide')
+    var list = document.querySelectorAll('.show-on-click')
+
     tabElms.forEach(function(tabElm) {
         tabElm.addEventListener('shown.bs.tab', function (event) {
-            var nakki = $(this).data('index');
-            $('.card-hide').addClass('d-none').filter('#card-'+nakki).removeClass('d-none');
-            $('.show-on-click').removeClass('d-none');
+            var nakki = event.target.dataset.index;
+            cards.forEach(function(card) {
+                card.classList.add('d-none')
+                console.log(card.id);
+                if(card.id == 'card-'+nakki){
+                    card.classList.remove('d-none')
+                }
+                list[0].classList.remove('d-none')
+            });
         })
     })
 });
