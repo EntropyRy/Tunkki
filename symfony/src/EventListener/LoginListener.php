4c549d0859c7cf614e35730d47682eb04c409f11
Harri Häivälä <haivala@cc.hut.fi>
2022-12-31 12:07:30 +0200

    db upgrade

--- symfony/src/EventListener/LoginListener.php
+++ symfony/src/EventListener/LoginListener.php
@@ -5,10 +5,6 @@ namespace App\EventListener;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
-use Symfony\Component\Security\Http\Authenticator\Passport\UserPassportInterface;
 use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
 
 class LoginListener implements EventSubscriberInterface
@@ -17,7 +13,7 @@ class LoginListener implements EventSubscriberInterface
         private readonly EntityManagerInterface $em
     ) {
     }
-    public function onLoginSuccess(LoginSuccessEvent $event)
+    public function onLoginSuccess(LoginSuccessEvent $event): void
     {
         $user = $event->getUser();
         $request = $event->getRequest();
@@ -31,7 +27,7 @@ class LoginListener implements EventSubscriberInterface
         $this->em->persist($user);
         $this->em->flush();
     }
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [
             LoginSuccessEvent::class => 'onLoginSuccess'
