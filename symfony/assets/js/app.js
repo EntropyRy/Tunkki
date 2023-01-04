c7ec53241143d920ef68f1777dadf72ebe11500b
Harri Häivälä <haivala@cc.hut.fi>
2023-01-04 13:22:15 +0200

    optimizations

--- symfony/assets/js/app.js
+++ symfony/assets/js/app.js
@@ -1,30 +1,26 @@
-import '../styles/bs.scss';
-import '../styles/polaroid.scss';
-import '../styles/app.scss';
-import '../styles/dark.scss';
-import '../styles/animations.scss';
+import "../styles/bs.scss";
+import "../styles/polaroid.scss";
+import "../styles/app.scss";
+import "../styles/dark.scss";
+import "../styles/animations.scss";
 
 //import 'bootstrap';
-import 'bootstrap/js/dist/alert';
-import 'bootstrap/js/dist/button';
+import "bootstrap/js/dist/alert";
+import "bootstrap/js/dist/button";
 // import 'bootstrap/js/dist/carousel';
-import 'bootstrap/js/dist/collapse';
-import 'bootstrap/js/dist/dropdown';
+import "bootstrap/js/dist/collapse";
+import "bootstrap/js/dist/dropdown";
 // import 'bootstrap/js/dist/modal';
 // import 'bootstrap/js/dist/popover';
 // import 'bootstrap/js/dist/scrollspy';
-import 'bootstrap/js/dist/tab';
+import "bootstrap/js/dist/tab";
 // import 'bootstrap/js/dist/toast';
 // import 'bootstrap/js/dist/tooltip';
 
-require('@fortawesome/fontawesome-free/css/fontawesome.min.css');
-require('@fortawesome/fontawesome-free/css/solid.min.css');
-require('@fortawesome/fontawesome-free/css/brands.min.css');
 //require('@fortawesome/fontawesome-free/css/regular.min.css');
 //require('@fortawesome/fontawesome-free/js/brands.js');
 //require('@fortawesome/fontawesome-free/js/solid.js');
 //require('@fortawesome/fontawesome-free/js/fontawesome.js');
-import '@fontsource/space-grotesk/400.css';
-import '@fontsource/space-grotesk/700.css';
+import "@fontsource/space-grotesk/400.css";
+import "@fontsource/space-grotesk/700.css";
 //import '@fontsource/space-grotesk/500.css';
-
