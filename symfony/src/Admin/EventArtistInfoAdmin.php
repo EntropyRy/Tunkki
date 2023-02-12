0b956cd5e74e9b4453bfe8cb2b66d30673759054
Harri Häivälä <haivala@cc.hut.fi>
2023-02-12 11:38:11 +0200

    artist editor change

--- symfony/src/Admin/EventArtistInfoAdmin.php
+++ symfony/src/Admin/EventArtistInfoAdmin.php
@@ -28,8 +28,7 @@ final class EventArtistInfoAdmin extends AbstractAdmin
             ->add('Artist')
             ->add('SetLength')
             ->add('stage')
-            ->add('StartTime')
-        ;
+            ->add('StartTime');
     }
 
     protected function configureListFields(ListMapper $listMapper): void
@@ -39,19 +38,18 @@ final class EventArtistInfoAdmin extends AbstractAdmin
                 'template' => 'admin/crud/list__update_artist.html.twig'
             ])
             ->add('ArtistName')
+            ->add('ArtistClone.type')
             ->add('ArtistClone.hardware')
-            ->add('ArtistClone.linkUrls', FieldDescriptionInterface::TYPE_HTML, [
-            ])
+            ->add('ArtistClone.linkUrls', FieldDescriptionInterface::TYPE_HTML, [])
             ->add('ArtistClone.genre')
-            ->add('ArtistClone.member')
+            ->add('Artist.member')
             ->add('WishForPlayTime')
             ->add('freeWord')
             ->add('stage')
-            ->add('SetLength')
             ->add('StartTime')
             ->add(ListMapper::NAME_ACTIONS, null, [
                 'actions' => [
-//                    'show' => [],
+                    //                    'show' => [],
                     'edit' => [],
                     'delete' => [],
                     'update' => [
@@ -70,7 +68,7 @@ final class EventArtistInfoAdmin extends AbstractAdmin
                     'query_builder' => fn ($repo) => $repo->createQueryBuilder('a')
                         ->andWhere('a.copyForArchive = :copy')
                         ->setParameter('copy', false),
-                    'choice_label' => fn (Artist $artist) => $artist->getGenre() ? $artist->getName().' ('.$artist->getGenre().')' : $artist->getName(),
+                    'choice_label' => fn (Artist $artist) => $artist->getGenre() ? $artist->getName() . ' (' . $artist->getGenre() . ')' : $artist->getName(),
                 ]);
         } else {
             $formMapper
@@ -84,8 +82,7 @@ final class EventArtistInfoAdmin extends AbstractAdmin
                     'format' => 'd.M.y H:mm',
                     'required' => false,
                     'help' => 'Please select right date so that we can have right order in the timetable. This also tells the artist they have been chosen to play in their profile page'
-                ])
-            ;
+                ]);
         }
     }
 
@@ -93,8 +90,7 @@ final class EventArtistInfoAdmin extends AbstractAdmin
     {
         $showMapper
             ->add('SetLength')
-            ->add('StartTime')
-        ;
+            ->add('StartTime');
     }
     public function prePersist($eventinfo): void
     {
@@ -102,18 +98,18 @@ final class EventArtistInfoAdmin extends AbstractAdmin
         $i = 1;
         foreach ($event->getEventArtistInfos() as $info) {
             if ($info->getArtist() == $eventinfo->getArtist()) {
-                $i+=1;
+                $i += 1;
             }
         }
         $artistClone = clone $eventinfo->getArtist();
         $artistClone->setMember(null);
         $artistClone->setCopyForArchive(true);
-        $artistClone->setName($artistClone->getName().' for '.$eventinfo->getEvent()->getName(). ' #'. $i);
+        $artistClone->setName($artistClone->getName() . ' for ' . $eventinfo->getEvent()->getName() . ' #' . $i);
         $eventinfo->setArtistClone($artistClone);
     }
     protected function configureRoutes(RouteCollectionInterface $collection): void
     {
         $collection->remove('delete');
-        $collection->add('update', $this->getRouterIdParameter().'/update');
+        $collection->add('update', $this->getRouterIdParameter() . '/update');
     }
 }
