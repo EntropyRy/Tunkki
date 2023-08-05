52a91876ab1681e8398258d822fc0eb79a1cecab
Harri Häivälä <haivala@cc.hut.fi>
2023-08-05 15:14:19 +0300

    bug fix

--- symfony/src/Entity/Event.php
+++ symfony/src/Entity/Event.php
@@ -1058,7 +1058,7 @@ body {
     {
         $bookings = [];
         foreach ($this->getNakkis() as $nakki) {
-            if ($nakki->getResponsible() == $member) { // || in_array('ROLE_SUPER_ADMIN', $member->getUser()->getRoles())) {
+            if ($nakki->getResponsible() == $member || in_array('ROLE_SUPER_ADMIN', $member->getUser()->getRoles())) {
                 $bookings[$nakki->getDefinition()->getName($member->getLocale())]['b'][] = $nakki->getNakkiBookings();
                 $bookings[$nakki->getDefinition()->getName($member->getLocale())]['mattermost'] = $nakki->getMattermostChannel();
                 $bookings[$nakki->getDefinition()->getName($member->getLocale())]['responsible'] = $nakki->getResponsible();
