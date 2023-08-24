b215372ad8b49473e09824f34ab4c3fea558abba
Harri Häivälä <haivala@cc.hut.fi>
2023-08-24 11:55:39 +0300

    make stream controller 404 free

--- symfony/assets/controllers/stream_controller.js
+++ symfony/assets/controllers/stream_controller.js
@@ -20,23 +20,30 @@ export default class extends Controller {
   }
   streamStatus() {
     fetch(this.urlValue)
-      .then((response) => {
-        if (!response.ok) {
-          //throw new Error("Network response was not ok");
-          if (response.status === 404) {
-            console.log("Stream is not on!");
-          } else {
-            console.log("Network error!");
-          }
-        }
-        return response.status;
-        //this.setStream(response.status);
-      })
+      .then((response) => response.text())
       .then((data) => {
-        this.setStream(data);
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(data, "text/xml");
+        const sourceNode = xmlDoc.querySelector("source");
+
+        if (sourceNode) {
+          const sourceMount = sourceNode.getAttribute("mount");
+          const sourceListeners =
+            sourceNode.querySelector("listeners").textContent;
+
+          if (sourceMount) {
+            console.log("Icecast server is streaming on mount:", sourceMount);
+            this.setStream(200, sourceListeners);
+          } else {
+            console.log("Icecast server is not currently streaming");
+            this.setStream(404, 0);
+          }
+        } else {
+          this.setStream(404, 0);
+        }
       })
       .catch((error) => {
-        this.setStream(400);
+        this.setStream(500, 0);
         this.stopRefreshing();
       });
   }
@@ -54,10 +61,9 @@ export default class extends Controller {
       clearInterval(this.refreshTimer);
     }
   }
-  setStream(data) {
-    // console.log(data);
-    if (data == 200) {
-      this.badgeTarget.innerText = "ONLINE";
+  setStream(code, listeners) {
+    if (code == 200) {
+      this.badgeTarget.innerText = "ONLINE:" + listeners;
       this.badgeTarget.classList.add("bg-success");
       this.playerTarget.classList.remove("d-none");
       this.picTarget.setAttribute("src", this.onlineImgValue);
