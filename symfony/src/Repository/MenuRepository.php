0fb66cc69cbf7ea58a26712b61abcc6ad897d709
Harri Häivälä <haivala@cc.hut.fi>
2023-09-20 12:24:41 +0300

    fix depr

--- symfony/src/Repository/MenuRepository.php
+++ symfony/src/Repository/MenuRepository.php
@@ -3,10 +3,9 @@
 namespace App\Repository;
 
 use App\Entity\Menu;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepositoryInterface;
 use Gedmo\Tree\Entity\Repository\NestedTreeRepository;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Menu|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +13,7 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method Menu[]    findAll()
  * @method Menu[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class MenuRepository extends NestedTreeRepository
+class MenuRepository extends NestedTreeRepository implements ServiceEntityRepositoryInterface
 {
         public function __construct(EntityManagerInterface $manager)
         {
@@ -24,28 +23,28 @@ class MenuRepository extends NestedTreeRepository
         //  * @return Menu[] Returns an array of Menu objects
         //  */
         /*
-        public function findByExampleField($value)
-        {
-        return $this->createQueryBuilder('m')
-            ->andWhere('m.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('m.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-        }
-        */
+    public function findByExampleField($value)
+    {
+    return $this->createQueryBuilder('m')
+    ->andWhere('m.exampleField = :val')
+    ->setParameter('val', $value)
+    ->orderBy('m.id', 'ASC')
+    ->setMaxResults(10)
+    ->getQuery()
+    ->getResult()
+    ;
+    }
+    */
 
         /*
-        public function findOneBySomeField($value): ?Menu
-        {
-        return $this->createQueryBuilder('m')
-            ->andWhere('m.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-        }
-        */
+    public function findOneBySomeField($value): ?Menu
+    {
+    return $this->createQueryBuilder('m')
+    ->andWhere('m.exampleField = :val')
+    ->setParameter('val', $value)
+    ->getQuery()
+    ->getOneOrNullResult()
+    ;
+    }
+    */
 }
