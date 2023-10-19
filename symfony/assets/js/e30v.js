197519e61a64248c54d495bd6bf55735b85679cc
Harri Häivälä <haivala@cc.hut.fi>
2023-10-19 16:25:14 +0300

    change e30v font to Kanit

--- symfony/assets/js/e30v.js
+++ symfony/assets/js/e30v.js
@@ -16,4 +16,4 @@ import "bootstrap/js/dist/tab";
 //require('@fortawesome/fontawesome-free/js/brands.js');
 //require('@fortawesome/fontawesome-free/js/solid.js');
 //require('@fortawesome/fontawesome-free/js/fontawesome.js');
-import "@fontsource-variable/syne";
+import "@fontsource/kanit";
