a66b9ebc1a4d6d5dec4e098f4b0f2179e313dbf3
Harri Häivälä <haivala@cc.hut.fi>
2023-11-15 10:26:23 +0200

    make marque permanent with page loads

--- symfony/assets/e30v.js
+++ symfony/assets/e30v.js
@@ -1,13 +1,53 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
-//import './styles/app.css';
-
 // start the Stimulus application
 import "./bootstrap";
 import "./js/e30v.js";
+
+import Idiomorph from "idiomorph";
+import {
+  shouldPerformTransition,
+  performTransition,
+} from "turbo-view-transitions";
+
+let prevPath = window.location.pathname;
+
+const morphRender = (prevEl, newEl) => {
+  return Idiomorph.morph(prevEl, newEl, {
+    callbacks: {
+      beforeNodeMorphed: (fromEl, toEl) => {
+        if (typeof fromEl !== "object" || !fromEl.hasAttribute) return true;
+        if (fromEl.isEqualNode(toEl)) return false;
+
+        if (
+          fromEl.hasAttribute("data-morph-permanent") &&
+          toEl.hasAttribute("data-morph-permanent")
+        ) {
+          return false;
+        }
+
+        return true;
+      },
+    },
+  });
+};
+document.addEventListener("turbo:before-render", (event) => {
+  Turbo.navigator.currentVisit.scrolled = prevPath === window.location.pathname;
+  prevPath = window.location.pathname;
+
+  event.detail.render = async (prevEl, newEl) => {
+    await new Promise((resolve) => setTimeout(() => resolve(), 0));
+    await morphRender(prevEl, newEl);
+  };
+
+  if (shouldPerformTransition()) {
+    // Make sure rendering is synchronous in this case
+    event.detail.render = (prevEl, newEl) => {
+      morphRender(prevEl, newEl);
+    };
+
+    event.preventDefault();
+
+    performTransition(document.body, event.detail.newBody, async () => {
+      await event.detail.resume();
+    });
+  }
+});
