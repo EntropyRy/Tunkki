6ab0cc0695b3acd2b8238519f9e4a3ed85a09255
Harri Häivälä <haivala@cc.hut.fi>
2023-12-28 17:37:29 +0200

    update and login locale listener

--- symfony/src/EventListener/LoginListener.php
+++ symfony/src/EventListener/LoginListener.php
@@ -5,28 +5,34 @@ namespace App\EventListener;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Translation\LocaleSwitcher;
 use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
 
 class LoginListener implements EventSubscriberInterface
 {
     public function __construct(
+        private RequestStack $requestStack,
+        private LocaleSwitcher $localeSwitcher,
         private readonly EntityManagerInterface $em
     ) {
     }
     public function onLoginSuccess(LoginSuccessEvent $event): void
     {
+        // get user
         $user = $event->getUser();
         assert($user instanceof User);
-        $request = $event->getRequest();
         $user->setLastLogin(new \DateTime());
-        $request->setLocale($user->getMember()->getLocale());
         $this->em->persist($user);
         $this->em->flush();
+        // set user locale in session
+        $userLocale = $user->getMember()->getLocale() ?? 'fi';
+        $this->localeSwitcher->setLocale($userLocale);
     }
     public static function getSubscribedEvents(): array
     {
         return [
-            LoginSuccessEvent::class => 'onLoginSuccess'
+            LoginSuccessEvent::class => 'onLoginSuccess',
         ];
     }
 }
