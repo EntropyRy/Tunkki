e1619944cd019805729ff8a6ecaddcbc38bdc2b1
Harri Häivälä <haivala@cc.hut.fi>
2024-11-13 14:26:29 +0200

    move to scss

--- symfony/assets/app.js
+++ symfony/assets/app.js
@@ -6,9 +6,6 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import "./styles/app.css";
-import "./styles/animations.css";
-import "./styles/polaroid.css";
 import "@fontsource-variable/space-grotesk/index.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 // start the Stimulus application
