8e751c38d891bedd647baa26290a08d639d394be
Harri Häivälä <haivala@cc.hut.fi>
2024-12-15 11:43:16 +0200

    updates, add rector for update

--- ci/update-dev.sh
+++ ci/update-dev.sh
@@ -8,3 +8,4 @@ $dc exec fpm composer update;
 $dc exec fpm ./bin/console importmap:update; 
 $dc exec fpm ./vendor/bin/phpstan analyse src --level=5
 $dc exec fpm ./vendor/bin/twig-cs-fixer fix --fix templates/;
+$dc exec fpm ./vendor/bin/rector process src;
--- symfony/importmap.php
+++ symfony/importmap.php
@@ -81,19 +81,19 @@ return [
         'version' => '1.15.6',
     ],
     'intl-messageformat' => [
-        'version' => '10.7.8',
+        'version' => '10.7.10',
     ],
     'tslib' => [
         'version' => '2.8.1',
     ],
     '@formatjs/icu-messageformat-parser' => [
-        'version' => '2.9.5',
+        'version' => '2.9.7',
     ],
     '@formatjs/fast-memoize' => [
-        'version' => '2.2.4',
+        'version' => '2.2.5',
     ],
     '@formatjs/icu-skeleton-parser' => [
-        'version' => '1.8.9',
+        'version' => '1.8.11',
     ],
     '@hotwired/turbo' => [
         'version' => '8.0.12',
