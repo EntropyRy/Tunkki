55224067a9f0f361fd4e8cd1d7ade88182cbdc05
Harri Häivälä <haivala@cc.hut.fi>
2025-01-31 16:03:04 +0200

    make epic chanmge smooth with fade outs/ins

--- symfony/assets/controllers/epics_controller.js
+++ symfony/assets/controllers/epics_controller.js
@@ -5,21 +5,24 @@ import moment from "moment/min/moment-with-locales.min.js";
 export default class extends Controller {
   static targets = ["pic", "badge", "progress"];
   static values = { url: String, refreshInterval: Number };
+
   connect() {
     this.changePic();
-
     if (this.hasRefreshIntervalValue) {
       this.startRefreshing();
     }
   }
+
   disconnect() {
     this.stopRefreshing();
   }
+
   changePic() {
     fetch(this.urlValue)
       .then((response) => response.json())
       .then((data) => this.setPic(data));
   }
+
   startRefreshing() {
     if (!document.hidden) {
       this.refreshTimer = setInterval(() => {
@@ -29,28 +32,74 @@ export default class extends Controller {
       this.stopRefreshing();
     }
   }
+
   stopRefreshing() {
     if (this.refreshTimer) {
       clearInterval(this.refreshTimer);
     }
   }
+
   setPic(data) {
-    //console.log(data['taken']);
     if (data["taken"]) {
       let taken = moment(data["taken"]);
       this.badgeTarget.innerText = taken.format("D.M.yyyy, HH:mm");
     } else {
       this.badgeTarget.innerText = "";
     }
+
     if (data["url"]) {
+      // Remove shimmer class if it exists when showing actual image
       this.picTarget.classList.remove("shimmer");
-      this.picTarget.setAttribute("src", data["url"]);
-      this.progressTarget.animate([{ width: "100%" }, { width: "0%" }], {
-        duration: this.refreshIntervalValue,
-      });
+
+      // Create a new image element to preload
+      const newImage = new Image();
+      newImage.src = data["url"];
+
+      newImage.onload = () => {
+        // Start fade out of current image
+        const fadeOutAnimation = this.picTarget.animate(
+          [{ opacity: 1 }, { opacity: 0 }],
+          {
+            duration: 500,
+            easing: "ease",
+            fill: "forwards",
+          },
+        );
+
+        fadeOutAnimation.onfinish = () => {
+          this.picTarget.setAttribute("src", data["url"]);
+
+          // Fade in new image
+          this.picTarget.animate([{ opacity: 0 }, { opacity: 1 }], {
+            duration: 500,
+            easing: "ease",
+            fill: "forwards",
+          });
+
+          // Animate progress bar
+          this.progressTarget.animate([{ width: "100%" }, { width: "0%" }], {
+            duration: this.refreshIntervalValue,
+            easing: "linear",
+            fill: "forwards",
+          });
+
+          // Set up the fade out for next change
+          if (this.fadeOutTimer) {
+            clearTimeout(this.fadeOutTimer);
+          }
+
+          this.fadeOutTimer = setTimeout(() => {
+            this.picTarget.animate([{ opacity: 1 }, { opacity: 0 }], {
+              duration: 500,
+              easing: "ease",
+            });
+          }, this.refreshIntervalValue - 500);
+        };
+      };
     } else {
-      this.picTarget.classList.add("shimmer");
+      // Only add shimmer class when showing the header logo
       this.picTarget.setAttribute("src", "/images/header-logo.svg");
+      this.picTarget.classList.add("shimmer");
     }
   }
 }
