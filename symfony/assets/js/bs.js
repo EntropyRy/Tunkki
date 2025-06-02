7935f1d8b384ef425131309ac74e57ca4fd8d862
Harri Häivälä <haivala@cc.hut.fi>
2025-06-02 23:19:30 +0300

    try to convert everything to vanilla css

--- symfony/assets/js/bs.js
+++ symfony/assets/js/bs.js
@@ -1,5 +1,7 @@
-import { Alert } from "bootstrap";
-import { Button } from "bootstrap";
-import { Collapse } from "bootstrap";
-import { Dropdown } from "bootstrap";
-import { Tab } from "bootstrap";
+import * as bootstrap from 'bootstrap';
+
+const Alert = bootstrap.Alert;
+const Button = bootstrap.Button;
+const Collapse = bootstrap.Collapse;
+const Dropdown = bootstrap.Dropdown;
+const Tab = bootstrap.Tab;
