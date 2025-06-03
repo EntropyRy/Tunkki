9ba4c8c718b3f3e92d9a1384e907d4e20414be5d
Harri Häivälä <haivala@cc.hut.fi>
2025-06-03 23:33:32 +0300

    epics proxy

--- symfony/src/Controller/EPicsController.php
+++ symfony/src/Controller/EPicsController.php
@@ -4,27 +4,14 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Helper\ePics;
 
 class EPicsController extends AbstractController
 {
     #[Route('/api/epics/random', name: 'epics_random_pic')]
-    public function getRandomPic(Request $request, ePics $epics): JsonResponse
+    public function getRandomPic(ePics $epics): JsonResponse
     {
-        $data = $epics->getRandomPic();
-        $response = new JsonResponse($data);
-        
-        // Set headers to prevent Cloudflare from caching this API response
-        $response->headers->set('Cache-Control', 'no-cache, no-store, must-revalidate');
-        $response->headers->set('Pragma', 'no-cache');
-        $response->headers->set('Expires', '0');
-        $response->headers->set('CF-Cache-Status', 'BYPASS');
-        
-        // Always allow browser to cache briefly to reduce load
-        $response->setMaxAge(5);
-        
-        return $response;
+        return new JsonResponse($epics->getRandomPic());
     }
 }
