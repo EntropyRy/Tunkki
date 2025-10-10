ea65286965b6a95814ee06d1b4a28edeb504d8fe
Harri Häivälä <haivala@cc.hut.fi>
2025-10-10 21:06:51 +0300

    add start at to the stream artist

--- symfony/src/Entity/StreamArtist.php
+++ symfony/src/Entity/StreamArtist.php
@@ -38,6 +38,7 @@ class StreamArtist implements \Stringable
 
     public function __construct()
     {
+        $this->startedAt = new \DateTimeImmutable();
         $this->createdAt = new \DateTimeImmutable();
     }
     public function getId(): ?int
