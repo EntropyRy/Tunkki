bd612910e854146f2129a26eab58ded7bcfcc562
Harri Häivälä <haivala@cc.hut.fi>
2025-10-18 10:59:33 +0300

    configure phpstan, remove fixtures

--- symfony/src/Command/CmsSeedCommand.php
+++ symfony/src/Command/CmsSeedCommand.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Command;
 
-use App\Entity\Sonata\SonataPagePage;
 use App\Entity\Sonata\SonataPageSite;
 use App\Factory\PageFactory;
 use App\Factory\SiteFactory;
