f25f9a706ffe07287ffd8c2a9591876d2923ff81
Harri Häivälä <haivala@cc.hut.fi>
2025-12-13 19:51:41 +0200

    convert StatisticsBlock and TicketsBlock to twig component

---
+++ symfony/src/Twig/Components/Statistics.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig\Components;
+
+use App\Repository\BookingRepository;
+use App\Repository\EventRepository;
+use App\Repository\MemberRepository;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent]
+final class Statistics
+{
+    public array $stats = [];
+
+    public function __construct(
+        private readonly MemberRepository $memberR,
+        private readonly BookingRepository $bookingR,
+        private readonly EventRepository $eventR,
+    ) {
+    }
+
+    public function mount(): void
+    {
+        $this->stats['block.stats.members'] = $this->memberR->countByMember();
+        $this->stats['block.stats.active_members'] = $this->memberR->countByActiveMember();
+        $this->stats['block.stats.bookings'] = $this->bookingR->countHandled();
+        $this->stats['block.stats.events'] = $this->eventR->countDone();
+    }
+}
---
+++ symfony/src/Twig/Components/Tickets.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig\Components;
+
+use App\Entity\Member;
+use App\Repository\TicketRepository;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent]
+final class Tickets
+{
+    public array $tickets = [];
+
+    public function __construct(
+        private readonly TicketRepository $tRepo,
+    ) {
+    }
+
+    public function mount(Member $member): void
+    {
+        $this->tickets = $this->tRepo->findMemberTickets($member);
+    }
+}
